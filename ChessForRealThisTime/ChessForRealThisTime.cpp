8be6fe1394904c6fdd9ae8dd7cb70d677d590569
Mohamed <amarxyasser987@gmail.com>
2025-12-14 13:32:47 +0200

    Add the Animation Class

--- ChessForRealThisTime/ChessForRealThisTime.cpp
+++ ChessForRealThisTime/ChessForRealThisTime.cpp
@@ -10,6 +10,46 @@ const int H = 800;
 float DEGTORAD = 0.017453f;
 
 
+class Animation
+{
+public:
+    float Frame, speed;
+    //Sprite sprite;
+    std::unique_ptr<Sprite> sprite;
+    std::vector<IntRect> frames;
+
+    Animation() : Frame(0), speed(0.f), sprite(nullptr) {} // this still gives error
+
+    Animation(Texture& t, int x, int y, int w, int h, int count, float Speed) // this i add is good
+    {
+        Frame = 0;
+        speed = Speed;
+
+        for (int i = 0; i < count; i++)
+            frames.push_back(IntRect({x + i * w, y}, { w, h }));
+        sprite = std::make_unique<Sprite>(t);
+        sprite->setTexture(t);
+        sprite->setOrigin(Vector2f(w / 2, h / 2));
+        sprite->setTextureRect(frames[0]);
+    } 
+
+
+    void update()
+    {
+        Frame += speed;
+        int n = frames.size();
+        if (Frame >= n) Frame -= n;
+        if (n > 0) sprite->setTextureRect(frames[int(Frame)]);
+    }
+
+    bool isEnd()
+    {
+        return Frame + speed >= frames.size();
+    }
+
+};
+
+
 
 int main()
 {
