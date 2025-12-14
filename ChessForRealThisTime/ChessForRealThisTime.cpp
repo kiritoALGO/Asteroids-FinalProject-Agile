03be3ed578d476de97530b28c51ba42e9216ae9a
Gamal Abd Elnaser <192200379@ecu.edu.eg>
2025-12-14 14:30:28 +0200

    Add Specialasteroid class with movement logic

--- ChessForRealThisTime/ChessForRealThisTime.cpp
+++ ChessForRealThisTime/ChessForRealThisTime.cpp
@@ -129,6 +129,28 @@ public:
 };
 
 
+class Specialasteroid : public Entity
+{
+public:
+    asteroid()
+    {
+        dx = rand() % 8 - 4;
+        dy = rand() % 8 - 4;
+        name = "asteroid";
+    }
+
+    void update()
+    {
+        x += dx;
+        y += dy;
+
+        if (x > W) x = 0;  if (x < 0) x = W;
+        if (y > H) y = 0;  if (y < 0) y = H;
+    }
+
+};
+
+
 class asteroid : public Entity
 {
 public:
