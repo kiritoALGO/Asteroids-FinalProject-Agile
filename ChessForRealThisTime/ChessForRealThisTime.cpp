83ed10b9a96869e58fdf3331a2c0f0bb21d14c7f
Mohamed <amarxyasser987@gmail.com>
2025-12-14 13:42:01 +0200

    Adding the Entity Class & the Bullet,asteroid entities classes

--- ChessForRealThisTime/ChessForRealThisTime.cpp
+++ ChessForRealThisTime/ChessForRealThisTime.cpp
@@ -50,6 +50,128 @@ public:
 };
 
 
+class Entity
+{
+public:
+    float x, y, dx, dy, R, angle;
+    bool life;
+    std::string name;
+    Animation *anim;
+
+    Entity()
+    {
+        life = 1;
+    }
+
+    void settings(Animation* a, int X, int Y, float Angle = 0, int radius = 1)
+    {
+        anim = a;
+        x = X; y = Y;
+        angle = Angle;
+        R = radius;
+    }
+
+    virtual void update() {};
+
+    void draw(RenderWindow& app)
+    {
+        anim->sprite->setPosition(Vector2f(x, y));
+        anim->sprite->setRotation(degrees(angle + 90));
+        app.draw(*anim->sprite);
+
+        CircleShape circle(R);
+        circle.setFillColor(Color(255, 0, 0, 170));
+        circle.setPosition(Vector2f(x, y));
+        circle.setOrigin(Vector2f(R, R));
+        //app.draw(circle);
+    }
+
+    virtual ~Entity() {};
+};
+
+class Entity
+{
+public:
+    float x, y, dx, dy, R, angle;
+    bool life;
+    std::string name;
+    Animation *anim;
+
+    Entity()
+    {
+        life = 1;
+    }
+
+    void settings(Animation* a, int X, int Y, float Angle = 0, int radius = 1)
+    {
+        anim = a;
+        x = X; y = Y;
+        angle = Angle;
+        R = radius;
+    }
+
+    virtual void update() {};
+
+    void draw(RenderWindow& app)
+    {
+        anim->sprite->setPosition(Vector2f(x, y));
+        anim->sprite->setRotation(degrees(angle + 90));
+        app.draw(*anim->sprite);
+
+        CircleShape circle(R);
+        circle.setFillColor(Color(255, 0, 0, 170));
+        circle.setPosition(Vector2f(x, y));
+        circle.setOrigin(Vector2f(R, R));
+        //app.draw(circle);
+    }
+
+    virtual ~Entity() {};
+};
+
+
+class asteroid : public Entity
+{
+public:
+    asteroid()
+    {
+        dx = rand() % 8 - 4;
+        dy = rand() % 8 - 4;
+        name = "asteroid";
+    }
+
+    void update()
+    {
+        x += dx;
+        y += dy;
+
+        if (x > W) x = 0;  if (x < 0) x = W;
+        if (y > H) y = 0;  if (y < 0) y = H;
+    }
+
+};
+
+
+class bullet : public Entity
+{
+public:
+    bullet()
+    {
+        name = "bullet";
+    }
+
+    void  update()
+    {
+        dx = cos(angle * DEGTORAD) * 6;
+        dy = sin(angle * DEGTORAD) * 6;
+        // angle+=rand()%7-3;  /*try this*/
+        x += dx;
+        y += dy;
+
+        if (x > W || x<0 || y>H || y < 0) life = 0;
+    }
+
+};
+
 
 int main()
 {
