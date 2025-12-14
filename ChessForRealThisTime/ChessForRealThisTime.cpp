c52909d66220f9c8adcf22527ec102dac83cfc29
Mohamed <amarxyasser987@gmail.com>
2025-12-14 14:10:30 +0200

    Adding the Player Classe And What Happen in Collision

--- ChessForRealThisTime/ChessForRealThisTime.cpp
+++ ChessForRealThisTime/ChessForRealThisTime.cpp
@@ -173,6 +173,58 @@ public:
 };
 
 
+
+
+class player : public Entity
+{
+public:
+    bool thrust;
+
+    player()
+    {
+        name = "player";
+    }
+
+    void update()
+    {
+        if (thrust)
+        {
+            dx += cos(angle * DEGTORAD) * 0.2;
+            dy += sin(angle * DEGTORAD) * 0.2;
+        }
+        else
+        {
+            dx *= 0.99;
+            dy *= 0.99;
+        }
+
+        int maxSpeed = 15;
+        float speed = sqrt(dx * dx + dy * dy);
+        if (speed > maxSpeed)
+        {
+            dx *= maxSpeed / speed;
+            dy *= maxSpeed / speed;
+        }
+
+        x += dx;
+        y += dy;
+
+        if (x > W) x = 0; if (x < 0) x = W;
+        if (y > H) y = 0; if (y < 0) y = H;
+    }
+
+};
+
+
+bool isCollide(Entity* a, Entity* b)
+{
+    return (b->x - a->x) * (b->x - a->x) +
+        (b->y - a->y) * (b->y - a->y) <
+        (a->R + b->R) * (a->R + b->R);
+}
+
+
+
 int main()
 {
     srand(time(0));
